6f6f575cc3bb06a9f78eb89c97973269e66b3bca
tayden.miller <tayden@ualberta.ca>
2021-08-10 16:07:40 -0600

    try it

--- exception_notification.gemspec
+++ exception_notification.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'exception_notification'
-  s.version = '6.1.32'
+  s.version = '6.1.33'
   s.summary = 'exception_notification'
   s.authors = 'exception_notification'
 
--- lib/exception_notifier/email_notifier.rb
+++ lib/exception_notifier/email_notifier.rb
@@ -170,9 +170,10 @@ module ExceptionNotifier
     private
 
     def mailer
-      @@logger.info base_options
-      @@logger.info base_options[:mailer_parent].constantize
-      @@logger.info base_options[:template_path]
+      logger = Logger.new(STDOUT)
+      logger.info base_options
+      logger.info base_options[:mailer_parent].constantize
+      logger.info base_options[:template_path]
       @mailer ||= Class.new(base_options[:mailer_parent].constantize).tap do |mailer|
         mailer.extend(EmailNotifier::Mailer)
         mailer.mailer_name = base_options[:template_path]
